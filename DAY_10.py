aad6f44df149fabf4d2fd34ff8977c14b7fdca1d
KonaNandu777 <162981707+KonaNandu777@users.noreply.github.com>
2024-03-21 15:50:39 +0530

    Add files via upload

--- DAY_10.py
+++ DAY_10.py
@@ -1,3 +1,48 @@
+'''def min_max(products):
+   
+    min_price_product = min(products, key=products.get)
+    max_price_product = max(products, key=products.get)
+    return min_price_product, max_price_product
+
+def products_start_with_s(products):
+    # Find products starting with 's' or 'S'
+    s_products = [product for product in products if product.lower().startswith('s')]
+    return s_products
+
+d1 = {"shirt": 1000, "pant": 1500, "Shoes": 900, "hankey": 30}
+
+min_price, max_price = min_max_priced_product(d1)
+print("Min priced product:", min_price)
+print("Max priced product:", max_price)
+
+s_products = products_start_with_s(d1)
+print("Products starting with 's' or 'S':", s_products)'''
+
+
+'''def isStrong(n: int) ->bool:
+    sum = 0;
+    temp = n
+    while temp:
+        rem = temp%10
+        x = 1
+        facto = 1
+        while x <= rem:
+            facto *= x
+            x += 1
+            sum += facto
+            temp //= 10
+     if sum == n:
+        return True
+    else:
+        return False
+num = int(input("Enter a number: "))
+ans = isStrong(num)
+if ans:
+    print(f"{num} is a strong number")
+else:
+    print(f"{num} is not a strong number")'''
+
+
 '''def number_list(lst, n):
     num = len(lst)- n
     rotated = lst[num:] + lst[:num]
@@ -106,7 +151,8 @@ print(len(s))'''
 s=suming()
 s.add(3,5)#error
 s.add(4,8,9)'''
-class summing:
+#ex2
+'''class summing:
     def add (self,a=None,b=None,c=None):
         if a!=None and b!=None and c!=None:
             print(a+b+c)
@@ -117,7 +163,144 @@ class summing:
 obj=summing()
 obj.add(2)
 obj.add(5,8)
-obj.add(2,9,6)
+obj.add(2,9,6)'''
+
+#--> Abstaction
+#the process of hiding the implimentation details is abstraction
+#Rules of Abstraction
+'''
+1)Abstract class cannot be instantiated
+2)from abc import ABC,abstractmethod
+3)subclass the normal class with ABC
+4)convert the normal method inside the abstract class to abstract method
+5)all child classes have to be subclassed with abstract class
+6)the abstract method should be present in the child classes  
+'''
+#ex1
+'''from abc import ABC,abstractmethod
+class shapes(ABC):
+    @abstractmethod
+    def sides(self):
+        print('All shapes have sides except circle')
+class triangles(shapes):
+    def triangle_sides(self):
+        print("3 sides")
+    def name(self):
+        print("I am triangle")
+    def sides(self):
+        pass
+class square(shapes):
+    def square(self):
+        print("4 sides")
+    def sides(self):
+        pass
+tr=triangles()
+tr.triangle_sides()
+tr.name()'''
+#ex2
+#super() -->used to acess the parent class method from child class method
+'''from abc import ABC,abstractmethod
+class c1(ABC):
+    @abstractmethod
+    def m1(self):
+        print("This is abstract class")
+class c2(c1):
+    def m2(self):
+        super().m1()
+        print("I am child 1")
+    def m1(self):
+        pass
+class2=c2()
+class2.m2()'''
+
+#ex3
+'''from abc import ABC,abstractmethod
+class password(ABC):
+    @abstractmethod
+    def pwd(self):
+        pswd="Nandu1305"
+        return pswd
+class login(password):
+    def validate(self,name,password):
+        if super().pwd()==password:
+            print("Welcome",name,' !!')
+            print("login Successful")
+        else:
+            print("please check the password")
+    def pwd(self):
+        pass
+    
+login =login()
+name=input("enter the name: ")
+pwd=input("Enter the password:  ")
+login.validate(name,pwd)'''
+#-->Encapsulation
+#ex1
+'''class car:
+    __name="BMW"#private variable
+c1=car()
+print(c1.name)#error
+c1.name="Audi"
+print(c1.name)#error'''
+#private variable
+'''class car:
+    __name="BMW"
+    print(__name)#BMW'''
+#Acessing private data outside the class
+'''class c1:
+    __phone="335646667"
+    def display(self):
+        print(self.__phone)
+c=c1()
+c.display()'''
+#declare private method
+#m1
+'''class class1:
+    def __m1(self):
+        print("I am private method")
+       
+c=class1()
+c.__m1()'''#error
+#m2
+'''class class1:
+    def __m1(self):
+        print("I am private method")
+    def __init__(self):
+        self.__m1()       
+c=class1()'''
+#ex
+'''class class1:
+    class class2:
+        name="nandu"
+        def display(self):
+            print(name)
+    obj1=class2()
+obj=class1()
+obj.obj1.display()'''
+
+
+'''d1 = {"shirt": 1000, "pant": 1500, "Shoes": 900, "hankey": 30}                    
+for val in d1:
+    if d1[val] == min(d1.values()):
+        print(val)
+for val in d1:
+    if d1[val] == max(d1.values()):
+        print(val)
+for val in d1:
+    if val.startswith('s') or val.startswith('S'):
+        print(val)'''
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
